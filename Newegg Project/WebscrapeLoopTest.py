3d8d6ea52cb04f405fe103a36343ca0fc9db4a0a
djw135 <31595883+djw135@users.noreply.github.com>
2021-01-07 23:49:31 -0600

    Refactored code to simply larger functions.

--- Newegg Project/WebscrapeLoopTest.py
+++ Newegg Project/WebscrapeLoopTest.py
@@ -1,86 +1,104 @@
 import sqlite3
+import requests
+from typing import List, Tuple, Any, Optional
 from time import sleep
 from random import randint
 from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen as uReq
 
 # Creating a db
 conn = sqlite3.connect('Newegg_Products.db')
 cursor = conn.cursor()
 
+
 # Creating a table function that creates tables for the searched items to be stored in
-def create_Table(table_name: str):
-    info = '''CREATE TABLE {} (
+
+
+def create_table(table_name: str):
+    """
+
+    :param table_name:
+    :return:
+    """
+    query = """
+        CREATE TABLE {} (
         brand TEXT,
         product_name TEXT,
         shipping TEXT,
         price REAL
-    )'''.format(table_name)
+    )""".format(table_name)
     cursor.execute("DROP TABLE IF EXISTS {}".format(table_name))
-    cursor.execute(info)
-    return info
+    cursor.execute(query)
 
-# Creating the webscrape funtion
-def get_Data(table_name):
-    # Set start page, ending page, and page increments/steps
-    pages = range(1, 4)
 
-    # For loop to iterate through each page 
-    url = 'https://www.newegg.com/p/pl?d={}&page='.format(table_name)
-    for page in pages:
-        my_url = url + str(page)
-        uClient = uReq(my_url)
-        ram_html = uClient.read()
-        uClient.close()
-        ram_soup = soup(ram_html, "html.parser")
+def get_page_data(table_name: str) -> List:
+    page_data: List = []
+    for page in range(1, 2):
+        url: str = 'https://www.newegg.com/p/pl?d={}&page={}'.format(table_name, page)
+        page_text = requests.get(url).content
+        page_data.append(page_text)
+        sleep(randint(3, 10))
+    return page_data
+
+
+def get_brand_name(container) -> Optional[str]:
+    brand_container = container.find_all("a", {"class": "item-brand"})
+    if len(brand_container) == 0:
+        return None
+    return brand_container[0].img["title"]
+
+
+def get_product_name(container) -> str:
+    title_container = container.findAll("a", {"class": "item-title"})
+    return title_container[0].text
+
+
+def get_shipping(container) -> str:
+    shipping_container = container.findAll("li", {"class": "price-ship"})
+    return shipping_container[0].text.strip()
+
+
+def get_product_price(container) -> str:
+    try:
+        price_container = container.findAll("li", {"class": "price-current"})
+        price: str = price_container[0].strong.text + price_container[0].sup.text
+        return price
+    except AttributeError:
+        return None
+
+
+def process_page_data(page_data: List[str]) -> List[Tuple[Any]]:
+    processed_data: List[Tuple[Any]] = []
+    for item in page_data:
+        ram_soup = soup(item, "html.parser")
         containers = ram_soup.findAll("div", {"class": "item-container"})
-        sleep(randint(3,10))
-
-        # Making a list to store the items
-        data_insert = []
-        # Iterate over each product and grab the information you want
         for container in containers:
-            try:
-                brand_container = container.find_all("a", {"class": "item-brand"})
-                brand = brand_container[0].img["title"]
-            except IndexError:
-                continue
+            brand: Optional[str] = get_brand_name(container=container)
+            product_name: str = get_product_name(container=container)
+            shipping: str = get_shipping(container=container)
+            product_price: Optional[str] = get_product_price(container=container)
+            processed_data.append((brand, product_name, shipping, product_price))
+    return processed_data
 
-            title_container = container.findAll("a", {"class": "item-title"})
-            product_name = title_container[0].text
 
-            shipping_container = container.findAll("li", {"class": "price-ship"})
-            shipping = shipping_container[0].text.strip()
-
-            try:
-                price_container = container.findAll("li", {"class": "price-current"})
-                price = price_container[0].strong.text + price_container[0].sup.text
-            except AttributeError:
-                continue
-            
-            # Storing information in a list
-            data_insert.append((brand, product_name, shipping, price))
-
-    return data_insert
-
-# creating the insert data function that inserts the scraped data into the database
 def insert_data(stored_data, table_name):
     for item in stored_data:
         cursor.execute('''INSERT INTO {} VALUES(?, ?, ?, ?)'''.format(table_name), item)
+
 
 # Run function to call all the functions and print when done
 def run():
     table_names = ["ram", "cpu", "ssd", "gpu"]
     for table_name in table_names:
-        create_Table(table_name)
-        stored_data = get_Data(table_name)
-        insert_data(stored_data, table_name)
+        create_table(table_name)
+        page_data: List[str] = get_page_data(table_name=table_name)
+        processed_data: List[Tuple] = process_page_data(page_data=page_data)
+        insert_data(processed_data, table_name)
     print("Complete.")
 
     conn.commit()
     conn.close()
 
+
 # Main
 if __name__ == '__main__':
     run()
-    
